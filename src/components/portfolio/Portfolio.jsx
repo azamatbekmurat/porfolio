ac64eeb3545a39841f4d59576d06d6a4893c0c4a
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2021-05-24 21:15:03 -0700

    add porfolios to the portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,42 @@ import "./portfolio.scss"
 function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            Portfolio
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+
+            <div className="container">
+                <div className="item">
+                    <img src="https://www.tdbank.com/exc/img/eg-banking/SmBiz-MoBank-Banner-Image.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.tdbank.com/exc/img/eg-banking/SmBiz-MoBank-Banner-Image.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.tdbank.com/exc/img/eg-banking/SmBiz-MoBank-Banner-Image.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.tdbank.com/exc/img/eg-banking/SmBiz-MoBank-Banner-Image.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.tdbank.com/exc/img/eg-banking/SmBiz-MoBank-Banner-Image.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.tdbank.com/exc/img/eg-banking/SmBiz-MoBank-Banner-Image.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
+
         </div>
     )
 }
